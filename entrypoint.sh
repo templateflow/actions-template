5dacadb11cb2e537529b5b4767c21853bbf637fd
Oscar Esteban <code@oscaresteban.es>
2021-08-31 20:45:12 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,22 +3,18 @@
 git config --global user.name "$1"
 git config --global user.email "$2"
 
-# Start ssh agent
-eval "$(ssh-agent -s)"
-
 # Create ~/.ssh folder
 mkdir -p $HOME/.ssh
 chmod 700 $HOME/.ssh
 # Create key file, with permissions
-echo $SECRET_KEY | install -m 600 /dev/stdin $HOME/.ssh/id_rsa
+echo "${SECRET_KEY}" | install -m 600 /dev/stdin $HOME/.ssh/id_rsa
 
-# Prepare ssh client settings
-echo "Host *" > $HOME/.ssh/config
-echo "  AddKeysToAgent yes" >> $HOME/.ssh/config
-echo "  IdentityFile ~/.ssh/id_rsa" >> $HOME/.ssh/config
+# Start ssh agent
+eval "$(ssh-agent -s)"
 
 # Add key to ssh agent
-ssh-add - <<< "${SECRET_KEY}"
+# ssh-add - <<< "${SECRET_KEY}"
+ssh-add $HOME/.ssh/id_rsa
 
 datalad install git@github.com:templateflow/templateflow.git
 cd templateflow/
