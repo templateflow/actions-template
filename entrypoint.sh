13a05664b81b60d1558c489f076fc7e88877efca
Oscar Esteban <code@oscaresteban.es>
2021-08-31 22:43:25 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ git config --global user.email "$2"
 # Create ~/.ssh folder
 mkdir -p $HOME/.ssh
 chmod 700 $HOME/.ssh
+
+# Ease github.com checking
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> $HOME/.ssh/config
+
 # Create key file, with permissions
 echo "${SECRET_KEY}" | install -m 600 /dev/stdin $HOME/.ssh/id_rsa
 
@@ -16,7 +20,7 @@ eval "$(ssh-agent -s)"
 ssh-add $HOME/.ssh/id_rsa
 
 # Add github as trusted host
-ssh-keyscan -H github.com >> $HOME/.ssh/known_hosts
+# ssh-keyscan -H github.com >> $HOME/.ssh/known_hosts
 
 datalad install git@github.com:templateflow/templateflow.git
 cd templateflow/
