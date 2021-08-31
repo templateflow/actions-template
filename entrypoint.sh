e368ae9faf5c569baccb7d163ea549cc78a3a19c
Oscar Esteban <code@oscaresteban.es>
2021-08-31 15:05:08 +0200

    enh: handle key

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,22 @@
 git config --global user.name "$1"
 git config --global user.email "$2"
 
-echo "My secret: \"$SACRED_SECRET\""
+# Start ssh agent
+eval "$(ssh-agent -s)"
+
+# Store key
+mkdir -p $HOME/.ssh
+chmod 700 $HOME/.ssh
+touch $HOME/.ssh/id
+chmod 600 $HOME/.ssh/id
+echo $SECRET_KEY > $HOME/.ssh/id
+
+# Prepare ssh client settings
+echo "Host *" > $HOME/.ssh/config
+echo "  AddKeysToAgent yes" >> $HOME/.ssh/config
+echo "  IdentityFile ~/.ssh/id" >> $HOME/.ssh/config
+
+ssh-add -K ~/.ssh/id
 
 datalad install git@github.com:templateflow/templateflow.git
 cd templateflow/
